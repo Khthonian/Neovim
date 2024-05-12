4c6352dd6f2dd7497dc3de8247d3a42b56a4a092
Khthonian <anansicorp@tuta.io>
2024-05-13 00:13:33 +0100

    feat(plugins): Add Copilot.lua
    Copilot.lua is the lua conversion for the Copilot.vim plugin. It's
    essentially the same, however it offers the nicer lua configurations.
    Personally, I've added it because I would prefer to have Copilot in cmp
    rather than making stupid ghost splashes in my code environment. The
    original plugin will remain, commented out, to allow to quickly switch
    between the options.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,7 +77,53 @@ return {
   },
 
   {
-    'github/copilot.vim',
-    lazy = false,
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      {
+        'zbirenbaum/copilot-cmp',
+        config = function()
+          require('copilot_cmp').setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'nvim_lua' },
+        { name = 'path' },
+        { name = 'copilot' },
+      },
+    },
   },
+
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup()
+    end,
+    opts = {
+      suggestion = {
+        enabled = false,
+      },
+      panel = {
+        enabled = false,
+      },
+    },
+  },
+
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+  },
+
+  -- {
+  --   'github/copilot.vim',
+  --   lazy = false,
+  -- },
 }
